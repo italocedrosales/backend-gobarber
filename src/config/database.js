11dc171c9eb38b3c52475e3df28fc5487155ccd2
Italo Cedro <italo.cedro.sales@gmail.com>
2019-10-17 23:15:09 -0300

    Configurando Sequelize

---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+  dialect: 'postgres',
+  host: 'localhost',
+  username: 'postgres',
+  password: 'db1010',
+  database: 'gobarber',
+  define: {
+    timestamp: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+};
