b16ccd5108933a28685c83ed6a560e275d689055
Italo Cedro <italo.cedro.sales@gmail.com>
2019-10-18 23:30:41 -0300

    Criando loader de models

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -12,7 +12,7 @@ class User extends Model {
       {
         sequelize,
       }
-      );
+    );
   }
 }
 
--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,16 @@
 import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => res.json({ message: 'Hello World' }));
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'Italo Cedro',
+    email: 'italo.cedro.sales@gmail.com',
+    password_hash: '1234567890',
+  });
+
+  return res.json(user);
+});
 
 export default routes;
