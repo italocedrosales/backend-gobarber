1142cc8cf6cf60dc2528f6ec6dd086bb86d3e8ad
Italo Cedro <italo.cedro.sales@gmail.com>
2019-11-07 22:47:40 -0300

    Configurando Nodemailer

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: '2525',
+  secure: false,
+  auth: {
+    user: '902e4014719b08',
+    pass: 'ce3d73ccab1406',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
