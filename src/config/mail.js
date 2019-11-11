de8f0e1b434a54df4c6f399f89153af88dd3e67d
Italo Cedro <italo.cedro.sales@gmail.com>
2019-11-10 21:07:51 -0300

    Variaveis de ambiente

--- src/config/mail.js
+++ src/config/mail.js
@@ -1,10 +1,10 @@
 export default {
-  host: 'smtp.mailtrap.io',
-  port: '2525',
+  host: process.env.MAIL_HOST,
+  port: process.env.MAIL_PORT,
   secure: false,
   auth: {
-    user: '902e4014719b08',
-    pass: 'ce3d73ccab1406',
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASS,
   },
   default: {
     from: 'Equipe GoBarber <noreply@gobarber.com>',
--- src/config/sentry.js
+++ src/config/sentry.js
@@ -1,3 +1,3 @@
 export default {
-  dsn: 'https://6e60fec7bea3451b9a90a44eb4d46f42@sentry.io/1814892',
+  dsn: process.env.SENTRY_DSN,
 };
